109bb745f6df0fb5b14bb990732d1776dfa33242
Abdul <textabdulhere@gmail.com>
2023-10-23 13:49:36 +0530

    Made changes in modal

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Selectors
 
 const VIEW_MODAL_BTN = document.querySelector(".view_modal_button");
-const MODAL = document.querySelector(".modal");
+const MODAL_WRAPPER = document.querySelector(".modal_wrapper");
+
 const CLOSE_BTN = document.querySelector(".close_button");
 
 // Events
 
 VIEW_MODAL_BTN.addEventListener("click", () => {
-  MODAL.classList.toggle("view");
+  MODAL_WRAPPER.classList.toggle("view");
 });
 
 CLOSE_BTN.addEventListener("click", () => {
-  MODAL.classList.remove("view");
+  MODAL_WRAPPER.classList.remove("view");
 });
