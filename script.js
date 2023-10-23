4489aeea123e65e9acc4e79bff5115569bc3a701
Abdul <textabdulhere@gmail.com>
2023-10-23 14:05:13 +0530

    Fixed bugs in modal

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Selectors
 
 const VIEW_MODAL_BTN = document.querySelector(".view_modal_button");
-const MODAL_WRAPPER = document.querySelector(".modal_wrapper");
+const MODAL = document.querySelector(".modal");
 
 const CLOSE_BTN = document.querySelector(".close_button");
 
 // Events
 
 VIEW_MODAL_BTN.addEventListener("click", () => {
-  MODAL_WRAPPER.classList.toggle("view");
+  MODAL.classList.toggle("view");
 });
 
 CLOSE_BTN.addEventListener("click", () => {
-  MODAL_WRAPPER.classList.remove("view");
+  MODAL.classList.remove("view");
 });
