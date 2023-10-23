4a4b575309e437350a16dd7d9517760779054b00
Abdul <textabdulhere@gmail.com>
2023-10-23 13:19:41 +0530

    Just for hosting

---
+++ modal.js
@@ -0,0 +1,15 @@
+// Selectors
+
+const VIEW_MODAL_BTN = document.querySelector(".view_modal_button");
+const MODAL = document.querySelector(".modal");
+const CLOSE_BTN = document.querySelector(".close_button");
+
+// Events
+
+VIEW_MODAL_BTN.addEventListener("click", () => {
+  MODAL.classList.toggle("view");
+});
+
+CLOSE_BTN.addEventListener("click", () => {
+  MODAL.classList.remove("view");
+});
